12f9598253972903a66fdb28a792f93f7da01dbe
Lucas Arrigoni <lukasarri999111@gmail.com>
2022-11-24 17:26:35 -0300

    DML ejercicio 1

--- ejercicio_1/ejercicio_1_DML.sql
+++ ejercicio_1/ejercicio_1_DML.sql
@@ -1,1 +1,145 @@
-SELECT * FROM Factura WHERE(idCliente_fk=2);
+/*
+Ejercicio 1
+
+Cliente(id, nombre, apellido, DNI, telefono, direccion)
+Factura (nroTicket, total, fecha, hora, idCliente(fk))
+Detalle(nroTicket, idProducto, cantidad, preciounitario)
+Producto(idProducto, descripcion, precio, nombreP, stock)
+*/
+
+
+/*
+1. Listar datos personales de clientes cuyo apellido comience con
+el string ‘Pe’. Ordenar por DNI.
+*/
+SELECT c.nombre, c.apellido, c.DNI, c.telefono, c.direccion
+FROM Cliente c
+WHERE (apellido LIKE "Pe%")
+ORDER BY c.DNI;
+
+
+/*
+2. Listar nombre, apellido, DNI, telefono y direccion de clientes
+que realizaron compras solamente durante 2018.
+*/
+SELECT c.nombre, c.apellido, c.DNI, c.telefono, c.direccion
+FROM Cliente c INNER JOIN Factura f ON (c.id = f.idCliente_fk)
+WHERE (f.fecha BETWEEN 2018-01-01 and 2018-12-31)
+EXCEPT ( 
+    SELECT c.nombre, c.apellido, c.DNI, c.telefono, c.direccion
+    FROM Cliente c INNER JOIN Factura f ON (c.id = f.idCliente_fk)
+    WHERE (f.fecha < 2018-01-01 and f.fecha > 2018-12-31)
+);
+
+
+/*
+3. Listar nombre, descripción, precio y stock de productos 
+vendidos al cliente con DNI:42133425, pero que no fueron
+vendidos a clientes de apellido "Gar".
+*/
+SELECT p.nombreP, p.descripcion, p.precio, p.stock
+FROM Producto p 
+    INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+    INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+    INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+WHERE (c.DNI = 42133425)
+EXCEPT  (
+    SELECT p.nombreP, p.descripcion, p.precio, p.stock
+    FROM Producto p 
+        INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+        INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+        INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+    WHERE (c.apellido = "Gar")
+);
+
+
+/*
+4. Listar nombre, descripción, precio y stock de productos no
+vendidos a clientes que tengan telefono con característica: 221
+(La característica está al comienzo del telefono). Ordenar por nombre.
+*/
+SELECT p.nombreP, p.descripcion, p.precio, p.stock
+FROM Cliente c
+    INNER JOIN Factura f ON (c.id = f.idCliente_fk)
+    INNER JOIN Detalle d ON (f.nroTicket = d.nroTicket)
+    INNER JOIN Producto p ON (d.idProducto = p.idProducto)
+WHERE NOT (c.telefono LIKE "221%")
+ORDER BY nombreP;
+
+
+/*
+5. Listar para cada producto: nombre, descripción, precio y cuantas veces
+fué vendido. Tenga en cuenta que puede no haberse vendido nunca el producto.
+*/
+SELECT p.nombreP, p.descripcion, p.precio, SUM(d.cantidad) as cantidadVendido
+FROM Producto p INNER JOIN Detalle d ON(p.idProducto = d.idProducto)
+GROUP BY p.idProducto, p.nombreP, p.descripcion, p.precio;
+
+/*
+6. Listar nombre, apellido, DNI, telefono y direccion de clientes
+que compraron los productos con nombre "nombre_1" y "nombre_2" pero nunca
+compraron el producto con nombre "nombre_3".
+*/
+SELECT nombre, apellido, DNI, telefono
+FROM Producto p 
+    INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+    INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+    INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+WHERE (nombreP = "nombre_1") AND (c.id IN (
+    SELECT c.id
+    FROM Producto p 
+        INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+        INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+        INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+    WHERE (nombreP = "nombre_2")
+    )
+) EXCEPT(
+    SELECT nombre, apellido, DNI, telefono
+    FROM Producto p 
+        INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+        INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+        INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+    WHERE (nombreP = "nombre_3")
+);
+
+
+/*
+7. Listar nroTicket, total, fecha, hora y DNI del cliente,
+de aquellas facturas donde se haya comprado el producto "nombre_3"
+o la factura tenga fecha de 2019.
+*/
+SELECT f.nroTicket, f.total, f.fecha, f.hora, c.DNI
+FROM Producto p 
+    INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+    INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+    INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+WHERE (p.nombreP = "nombre_3") OR (f.fecha BETWEEN 2019-01-01 AND 2019-12-31);
+
+
+/*
+9. Listar nroTicket, total, fecha, hora para las facturas del
+cliente "Luis Pérez" donde no haya comprado el producto "nombre_3".
+*/
+SELECT f.nroTicket, total, fecha, hora
+FROM Producto p 
+    INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+    INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+    INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+WHERE (nombre = "Luis") AND (apellido = "Perez") AND f.nroTicket NOT IN (
+    SELECT f.nroTicket
+    FROM Producto p
+        INNER JOIN Detalle d ON (p.idProducto = d.idProducto)
+        INNER JOIN Factura f ON (d.nroTicket = f.nroTicket) 
+        INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+    WHERE (p.nombreP = "nombre_3")
+);
+
+
+/*
+10. Listar DNI, apellido y nombre de clientes donde el monto total
+comprado, teniendo en cuenta todas sus facturas, supere $80.000.
+*/
+SELECT c.id, c.DNI, c.apellido, c.nombre
+FROM Factura f INNER JOIN Cliente c ON (f.idCliente_fk = c.id)
+GROUP BY c.id, c.DNI, c.apellido, c.nombre
+HAVING SUM (f.total) > 80000;
--- ejercicio_1/ejercicio_1_Datos.sql
+++ ejercicio_1/ejercicio_1_Datos.sql
@@ -10,8 +10,6 @@ INSERT INTO Cliente (id, nombre, apellido, DNI, telefono, direccion) VALUES
 (NULL, 'Lucas', 'Humer', 65437247, 2213465234, '54 n4573'),
 (5002, 'Jorge Luis', 'Castor', 40578999, 2214400789, '11 n2587');
 
-DELETE FROM Cliente WHERE(DNI=40123123);
-
 
 -- Agregar Facturas
 INSERT INTO Factura (nroTicket, total, fecha, hora, idCliente_fk) VALUES
@@ -24,9 +22,39 @@ INSERT INTO Factura (nroTicket, total, fecha, hora, idCliente_fk) VALUES
 (107, 45677.00, '2019-6-1', '04:35:00', 6),
 (108, 41244.90, '2021-3-2', '03:34:00', 7),
 (109, 43213.80, '2021-12-7', '02:10:00', 8),
-(110, 40232.70, '2020-11-5', '01:10:00', 9),
-(111, 10000.60, '2020-4-9', '02:10:00', 9),
-(112, 20000.95, '2019-6-5', '09:20:00', 9),
+(110, 40232.70, '2020-11-5', '01:10:00', 8),
+(111, 10000.60, '2020-4-9', '02:10:00', 4),
+(112, 20000.95, '2019-6-5', '09:20:00', 3),
 (113, 12323.88, '2018-10-19', '11:30:00', 5002);
 
-DELETE FROM Factura WHERE(nroTicket=100);
+
+-- Agregar Producto
+INSERT INTO Producto (idProducto, descripcion, precio, nombreP, stock) VALUES
+(901, 'buen producto', 3090.90, 'nombre_1', 400),
+(902, 'buen producto', 2342.10, 'nombre_2', 500),
+(903, 'buen producto', 4658.50, 'nombre_3', 70),
+(904, 'buen producto', 8765.50, 'nombre_4', 10),
+(905, 'buen producto', 2754.55, 'nombre_5', 79),
+(906, 'buen producto', 3464.99, 'nombre_6', 435),
+(907, 'buen producto', 4574.77, 'nombre_7', 432),
+(908, 'buen producto', 3654.23, 'nombre_8', 764),
+(909, 'buen producto', 2346.12, 'nombre_9', 34),
+(910, 'buen producto', 2354.10, 'nombre_10', 40),
+(911, 'buen producto', 3090.90, 'nombre_1', 400);
+
+
+-- Agregar Detalle
+INSERT INTO Detalle (nroTicket, idProducto, cantidad, precioUnitario) VALUES
+(100, 901, 3000, 4000.55),
+(102, 902, 8700, 3345.55),
+(103, 903, 3043, 7456.55),
+(104, 904, 1200, 3453.55),
+(105, 905, 6800, 7455.55),
+(106, 906, 4000, 6546.55),
+(107, 907, 3000, 3450.55),
+(108, 908, 2000, 1250.55),
+(109, 909, 3000, 1230.55),
+(110, 910, 3000, 7800.55),
+(111, 911, 3000, 7000.55),
+(112, 906, 3000, 6000.55),
+(113, 906, 3000, 5000.55);
