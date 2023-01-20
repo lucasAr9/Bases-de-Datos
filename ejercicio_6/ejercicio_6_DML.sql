23604a6342f8b4993be1ccab9736b040bf68cf5e
Lucas Arrigoni <lukasarri999111@gmail.com>
2023-01-20 16:58:31 -0300

    ejercicio 6 DML

--- ejercicio_6/ejercicio_6_DML.sql
+++ ejercicio_6/ejercicio_6_DML.sql
@@ -12,25 +12,55 @@ RepuestoReparacion (nroReparacion_fk, codRepuesto_fk, cantidad, precio)
 1. Listar todos los repuestos, informando el nombre, stock y precio.
 Ordenar el resultado por precio.
 */
-SELECT
-;
+SELECT r.codRepuesto, r.nombre, r.stock, r.precio
+FROM Repuesto r
+ORDER BY r.precio;
+
 
 /*
 2. Listar nombre, stock, precio de repuesto que participaron en reparaciones
 durante 2019 y ademas no participaron en reparaciones del tecnico 'Jose Perez'
 */
+SELECT repu.codRepuesto, repu.nombre, repu.stock, repu.precio
+FROM Repuesto repu
+    INNER JOIN RepuestoReparacion rr ON (repu.codRepuesto = rr.codRepuesto_fk)
+    INNER JOIN Reparacion r ON (rr.nroReparacion_fk = r.nroReparacion)
+    INNER JOIN Tecnico t ON (r.codTecnico_fk = t.codTecnico)
+WHERE (r.fecha BETWEEN '2019-01-01' AND '2019-12-31') AND t.codTecnico NOT IN (
+    SELECT t.codTecnico
+    FROM Tecnico t
+    WHERE (t.nombre = 'Jose Perez')
+);
 
 
 /*
-3. Listar el nombre y especialidad de tecnicos que que no participaron en
+3. Listar el nombre y especialidad de tecnicos que no participaron en
 ninguna reparacion. Ordenar por nombre ascendentemente.
 */
+SELECT t.codTecnico, t.nombre, t.especialidad
+FROM Tecnico t
+WHERE NOT EXISTS (
+    SELECT *
+    FROM Reparacion r
+    WHERE (r.codTecnico_fk = t.codTecnico)
+)
+ORDER BY t.nombre;
 
 
 /*
 4. Listar el nombre y especialidad de tecnicos que solo participaron en
 reparaciones durante 2020.
 */
+SELECT t.codTecnico, t.nombre, t.especialidad
+FROM Tecnico t
+    INNER JOIN Reparacion r ON (t.codTecnico = r.codTecnico_fk)
+WHERE (r.fecha BETWEEN '2020-01-01' AND '2020-12-31')
+EXCEPT (
+    SELECT t.codTecnico, t.nombre, t.especialidad
+    FROM Tecnico t
+        INNER JOIN Reparacion r ON (t.codTecnico = r.codTecnico_fk)
+    WHERE (r.fecha < '2020-01-01' AND r.fecha > '2020-12-31')
+);
 
 
 /*
@@ -38,34 +68,89 @@ reparaciones durante 2020.
 que lo utilizaron. Si un repuesto no participo en alguna reparacion, igual
 debe apareder en dicho listado.
 */
+SELECT r.nombre, r.stock, COUNT(DISTINCT repa.codTecnico_fk) as Cantidad_Tecnicos
+FROM Repuesto r
+    LEFT JOIN RepuestoReparacion rr ON (r.codRepuesto = rr.codRepuesto_fk)
+    INNER JOIN Reparacion repa ON (rr.nroReparacion_fk = repa.nroReparacion)
+GROUP BY r.codRepuesto, r.nombre, r.stock;
 
 
 /*
 6. Listar nombre y especialidad del tecnico con mayor cantidad de reparaciones
 realizadas y el tecnico con menor cantidad de reparaciones.
 */
+SELECT t.codTecnico, t.nombre, t.especialidad, COUNT(r.codTecnico_fk) as cantidad
+FROM Tecnico t INNER JOIN Reparacion r ON (r.codTecnico_fk = t.codTecnico)
+GROUP BY t.codTecnico, t.nombre, t.especialidad
+HAVING COUNT(*) >= ALL (
+    SELECT COUNT(*)
+    FROM Reparacion r
+    GROUP BY r.codTecnico_fk
+);
+SELECT t.codTecnico, t.nombre, t.especialidad, COUNT(r.codTecnico_fk) as cantidad
+FROM Tecnico t INNER JOIN Reparacion r ON (r.codTecnico_fk = t.codTecnico)
+GROUP BY t.codTecnico, t.nombre, t.especialidad
+HAVING COUNT(*) <= ALL (
+    SELECT COUNT(*)
+    FROM Reparacion r
+    GROUP BY r.codTecnico_fk
+);
 
 
 /*
-7. Listar nombre, stock y precio de todos los repuestos con stock mayor a 0 y
+7. Listar nombre, stock y precio de todos los repuestos con stock mayor a 10 y
 que dicho repuesto no haya estado en reparacion con precio total superior a 1000.
 */
+SELECT r.codRepuesto, r.nombre, r.stock, r.precio
+FROM Repuesto r
+    INNER JOIN RepuestoReparacion rr ON (r.codRepuesto = rr.codRepuesto_fk)
+    INNER JOIN Reparacion repa ON (rr.nroReparacion_fk = repa.nroReparacion)
+WHERE (r.stock > 10)
+INTERSECT (
+    SELECT r.codRepuesto, r.nombre, r.stock, r.precio
+    FROM Repuesto r
+        INNER JOIN RepuestoReparacion rr ON (r.codRepuesto = rr.codRepuesto_fk)
+        INNER JOIN Reparacion repa ON (rr.nroReparacion_fk = repa.nroReparacion)
+    WHERE (repa.precioTotal > 1000)
+);
 
 
 /*
 8. Proyectar precio, fecha y precio total de aquellas reparaciones donde se
-utilizo algun repuesto con precio en el momento de reparacion mayor a $1000
+utilizo algun repuesto con precio en el momento de reparacion, mayor a $1000
 y menor a $5000.
 */
+SELECT rr.precio, repa.fecha, repa.precioTotal
+FROM Reparacion repa
+    INNER JOIN RepuestoReparacion rr ON (repa.nroReparacion = rr.nroReparacion_fk)
+    INNER JOIN Repuesto r ON (r.codRepuesto = rr.codRepuesto_fk)
+WHERE (rr.precio >= 1000 AND rr.precio <= 5000);
 
 
 /*
 9. Listar nombre, stock y precio de repuestos que hayan sido utilizados en
 todas las reparaciones.
 */
+SELECT r.codRepuesto, r.nombre, r.stock, r.precio
+FROM Repuesto r
+WHERE NOT EXISTS (
+    SELECT *
+    FROM Reparacion repa
+    WHERE NOT EXISTS (
+        SELECT *
+        FROM RepuestoReparacion rr
+        WHERE (r.codRepuesto = rr.codRepuesto_fk) AND (repa.nroReparacion = rr.nroReparacion_fk)
+    )
+);
 
 
 /*
 10. Listar fecha, tecnico y precio total de aquellas reparaciones que
 necesitaron al menos 10 repuestos distintos.
 */
+SELECT repa.nroReparacion, repa.fecha, t.nombre, repa.precioTotal
+FROM Reparacion repa
+    INNER JOIN Tecnico t ON (repa.codTecnico_fk = t.codTecnico)
+    INNER JOIN RepuestoReparacion rr ON (repa.nroReparacion = rr.nroReparacion_fk)
+GROUP BY repa.nroReparacion, repa.fecha, t.nombre, repa.precioTotal
+HAVING (COUNT(*) > 10);
--- ejercicio_6/ejercicio_6_Datos.sql
+++ ejercicio_6/ejercicio_6_Datos.sql
@@ -1,14 +1,56 @@
 INSERT INTO Tecnico (codTecnico, nombre, especialidad) VALUES
-(1, 'La Plata', 'una descripcion', 4553123);
+(1, 'Jose Perez', 'reparacion'),
+(2, 'Luis Perez', 'reparacion'),
+(3, 'German Garmendia', 'reparacion'),
+(4, 'Nico Liz', 'inspector'),
+(5, 'Pepe Zipper', 'inspector'),
+(6, 'Juan Cient', 'inspector'),
+(7, 'Carlos Gar', 'inspector'),
+(8, 'Lucas Humer', 'refrigeracion de material de construccion'),
+(9, 'Jorge Luis Castor', 'refrigeracion de material de construccion');
 
 
 INSERT INTO Repuesto (codRepuesto, nombre, stock, precio) VALUES
-(1, 'La Plata', 'una descripcion', 4553123);
+(100, 'cauchos', 78, 423),
+(101, 'mangeras', 20, 4500),
+(102, 'llave', 5, 900),
+(103, 'alicate', 3, 200),
+(104, 'tornillo', 650, 100),
+(105, 'flotador', 43, 800),
+(106, 'sifones', 16, 540),
+(107, 'cheques', 7, 77),
+(108, 'llave final de media', 19, 4090),
+(109, 'arboles de entrada', 34, 700),
+(110, 'arboles de salida', 32, 700);
 
 
 INSERT INTO Reparacion (nroReparacion, codTecnico_fk, precioTotal, fecha) VALUES
-(1, 'La Plata', 'una descripcion', 4553123);
+(500, 1, 50400, '2019-8-10'),
+(501, 2, 40400, '2022-5-23'),
+(502, 3, 70234, '2022-6-12'),
+(503, 4, 642, '2022-4-21'),
+(504, 4, 23423, '2022-2-20'),
+(505, 4, 14555, '2021-9-12'),
+(506, 5, 12255, '2019-6-1'),
+(507, 6, 6436, '2021-3-2'),
+(508, 7, 60400, '2021-12-7'),
+(509, 7, 12333, '2020-11-5'),
+(510, 8, 8462, '2020-4-9'),
+(511, 7, 400, '2019-6-5'),
+(512, 6, 505, '2018-10-19');
 
 
 INSERT INTO RepuestoReparacion (nroReparacion_fk, codRepuesto_fk, cantidad, precio) VALUES
-(1, 'La Plata', 'una descripcion', 4553123);
+(500, 100, 4, 454),
+(501, 100, 6, 4642),
+(502, 100, 3, 2345),
+(503, 101, 8, 4153),
+(504, 102, 11, 6132),
+(505, 103, 4, 1111),
+(506, 103, 6, 2333),
+(507, 103, 56, 4444),
+(508, 106, 88, 645),
+(509, 107, 190, 1233),
+(510, 108, 23, 2523),
+(511, 109, 57, 223),
+(512, 110, 230, 350);
